dc81bb66e75d57fde480f0b0075e6db3f036d53a
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-22 09:01:26 -0400

    Update TPS-Report.sh

--- TPS-Report.sh
+++ TPS-Report.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 clear
 grep "Results for qwen:7b" .nosana/flows.json
-date +"%a %d%b%g %H:%M:%S"
+echo Watch Average Tokens per Second
 echo "TPS Report auto updates every 60 seconds. Last Update:  $(date +"%a %d%b%g %H:%M:%S")"
 read -t 60 -p "Press a key for instant update or Press ctrl-c to cancel."
 ./TPS-Report.sh
