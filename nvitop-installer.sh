710148f92a1ae44e510debc32aacf992202ecf62
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-22 16:19:25 -0400

    Update nvitop-installer.sh

--- nvitop-installer.sh
+++ nvitop-installer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /home/$SUDO_USER
+cd /home/$SUSER
 rm -r -f nvitop.sh
 sudo apt update -y
 sudo apt install python3-pip -y
@@ -7,10 +7,10 @@ sudo apt install git -y
 # coreutils installed for chmod on minimal installed Ubuntu
 sudo apt install coreutils -y
 git clone --depth=1 https://github.com/XuehaiPan/nvitop.git
-cd /home/$SUDO_USER/nvitop
+cd /home/$SUSER/nvitop
 pip3 install . --no-color
 pip3 install -r requirements.txt --no-color
-cd /home/$SUDO_USER
+cd /home/$SUSER
 wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/MachoDrone/UbDt2204modules/main/nvitop.sh
-chmod +x /home/$SUDO_USER/nvitop.sh
+chmod +x /home/$USER/nvitop.sh
 echo nvitop was installed or updated. Run with: ./nvitop.sh
