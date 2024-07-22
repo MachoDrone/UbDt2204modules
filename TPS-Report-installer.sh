56e733b0a71f9382398fd150a03a370fcbe785ff
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-22 12:32:21 -0400

    Update TPS-Report-installer.sh

--- TPS-Report-installer.sh
+++ TPS-Report-installer.sh
@@ -2,6 +2,7 @@
 cd /home/$USER
 rm -r -f TPS-Report.sh
 sudo apt update -y
+# coreutils installed for chmod on minimal installed Ubuntu
 sudo apt install coreutils -y
 sudo apt install wget -y
 wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/MachoDrone/UbDt2204modules/main/TPS-Report.sh
