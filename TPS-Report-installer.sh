a2b3fccb0451f3af320132b17e602d93f36b707b
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-22 09:14:44 -0400

    Update TPS-Report-installer.sh

--- TPS-Report-installer.sh
+++ TPS-Report-installer.sh
@@ -1,6 +1,6 @@
 #!/bin/ssh
 cd /home/$USER
-sudo rm -r -f TPS-Report.sh
+rm -r -f TPS-Report.sh
 wget https://raw.githubusercontent.com/MachoDrone/UbDt2204modules/main/TPS-Report.sh
-sudo chmod +x TPS-Report.sh
+chmod +x TPS-Report.sh
 echo TPS-Report was installed or updated
