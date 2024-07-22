fa4d23126ee4d2288e445312f9574e6c79b0ca52
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-22 09:45:26 -0400

    Update TPS-Report-installer.sh

--- TPS-Report-installer.sh
+++ TPS-Report-installer.sh
@@ -4,6 +4,6 @@ rm -r -f TPS-Report.sh
 sudo apt update -y
 sudo apt install coreutils -y
 sudo apt install wget -y
-wget https://raw.githubusercontent.com/MachoDrone/UbDt2204modules/main/TPS-Report.sh
+wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/MachoDrone/UbDt2204modules/main/TPS-Report.sh
 chmod +x TPS-Report.sh
 echo TPS-Report was installed or updated
