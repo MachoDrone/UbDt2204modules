e0a956efe820031e38c636293dc0ec19c2df6c1f
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-22 13:33:29 -0400

    Create nvitop-installer.sh

---
+++ nvitop-installer.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo apt install python3-pip -y
+sudo apt install git -y
+# coreutils installed for chmod on minimal installed Ubuntu
+sudo apt install coreutils -y
+git clone --depth=1 https://github.com/XuehaiPan/nvitop.git
+cd /home/$SUDO_USER/nvitop
+pip3 install . --no-color
+pip3 install -r requirements.txt --no-color
+cd /home/$SUDO_USER
+wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/MachoDrone/UbDt2204modules/main/nvitop.sh
+chmod +x /home/$SUDO_USER/nvitop.sh
