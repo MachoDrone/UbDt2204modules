a73cec44c60c879d9c2a6bd0fdbd67118903e18a
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-22 16:31:47 -0400

    Update nvitop-installer.sh

--- nvitop-installer.sh
+++ nvitop-installer.sh
@@ -10,7 +10,7 @@ git clone --depth=1 https://github.com/XuehaiPan/nvitop.git
 cd /home/$USER/nvitop
 pip3 install . --no-color
 pip3 install -r requirements.txt --no-color
-cd /home/$USER
+cd ..
 wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/MachoDrone/UbDt2204modules/main/nvitop.sh
 chmod +x /home/$USER/nvitop.sh
 echo nvitop was installed or updated. Run with: ./nvitop.sh
