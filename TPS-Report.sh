8094b324b56c0c82541ecc06b5ef1873c09106f0
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-22 08:59:11 -0400

    Update and rename tpview.sh to TPS-Report.sh

--- TPS-Report.sh
+++ TPS-Report.sh
@@ -2,6 +2,6 @@
 clear
 grep "Results for qwen:7b" .nosana/flows.json
 date +"%a %d%b%g %H:%M:%S"
-echo "tpsview auto updates every 60 seconds. Last Update:  $(date +"%a %d%b%g %H:%M:%S")"
+echo "TPS Report auto updates every 60 seconds. Last Update:  $(date +"%a %d%b%g %H:%M:%S")"
 read -t 60 -p "Press a key for instant update or Press ctrl-c to cancel."
-./tpsview.sh
+./TPS-Report.sh
