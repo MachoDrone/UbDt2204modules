e05e9a39c032108c7ff4f7861ef917a87da20dd5
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-23 04:11:45 -0400

    Update startnode-installer.sh

--- startnode-installer.sh
+++ startnode-installer.sh
@@ -7,6 +7,11 @@ sudo apt install coreutils -y
 sudo apt install wget -y
 wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/MachoDrone/UbDt2204modules/main/startnode.sh
 chmod +x startnode.sh
-echo "***********************************************************"
+echo " "
+echo "------------------------------------------------------------"
+echo "------------------------------------------------------------"
+echo " "
 echo startnode was installed or updated. Run with: ./startnode.sh
-echo "-----------------------------------------------------------"
+echo " "
+echo "------------------------------------------------------------"
+echo "------------------------------------------------------------"
