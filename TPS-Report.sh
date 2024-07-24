67c07b08bd43f9ff4079cdb186fa9e95a16a7246
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-24 15:42:25 -0400

    Update TPS-Report.sh

--- TPS-Report.sh
+++ TPS-Report.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 clear
-grep "Results for qwen:7b" .nosana/flows.json | tail -16
+grep "Results for llama" .nosana/flows.json | tail -16
 echo Watch Average Tokens per Second
 echo "TPS Report auto updates every 7 seconds. Last Update:  $(date +"%a %d%b%g %H:%M:%S")"
 sleep 7
